16efaa49337c91323262a2381bcbed3a3a43cb8b
gclotault <g.clotault@gmail.com>
2016-10-20 17:03:32 +0200

    - Création de functions.py contenant toutes les fonctions mathématiques - fit.py 	- import functions 	- modifications CI pour régler le bug du log - plot.py 	- import functions 	- plt.axis([min,max,0.,1.]) 	- suppression des if plot 	- régression lin - kcal.py 	- remplacement de reduce par math.round()

---
+++ functions.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def funcexp(x, a, b, c):			# function for the exponential regression
+    return a * np.exp(-b * x) + c
+
+
+def funcquad(x, a, b, c):			# function for the quadratic regression
+    return c * x - b * x**2 + a
+
+
+def funcpuis(x, a, b, c):			# function for the puissance regression
+  return a * ((x**(1 - b) - 1) / (1 - b)) + c
+
+def funclog(x, a, b, c, d):			# function for the logarithmic regression
+  return a * np.log(b * x + c) + d
+
+def funclin(x, a, b):				# function for the linear regression
+  return a * x + b
